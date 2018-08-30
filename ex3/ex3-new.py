de85becc5458d939e2e40f1333ed1a16af7c56b4
Antonio <antoniosivanovs@gmail.com>
2018-08-31 00:58:05 +0300

    Exercise 3 is done. page 46 reached

---
+++ ex3/ex3-new.py
@@ -0,0 +1,4 @@
+print("We have 328 pages of book and 30 days")
+print("I should read 328/30 pages a day")
+x=328/30
+print("328/30={0:0.2f}".format(x))
---
+++ ex3/ex3.py
@@ -0,0 +1,24 @@
+#PE(M&D)(A&S)
+print("I will now count my chicken")
+#Deviding 30/6 and then adding 25printing result.
+#25*3 then remainder(modulus) of the division of 75 to 4.
+#75 devided by 4 with 3 remaining
+print("Hens",25.0+30.0/6.0)
+print("Roosters",100.0-25.0*3.0%4.0)
+
+print("Now i Will count the eggs")
+#3+21-5+reminder of 4%2=0-0.25+6
+print(3.0+21.0-5.0+4.0%2.0-1.0/4.0+6.0)
+#5<-2 is false
+print("It's true that 3+2<5-7?")
+#prinnting result
+print(3+2<5-7)
+print("What is 3+2?",3+2)
+print("What is 5-7?",5-7)
+print("Oh that's why it's False")
+
+print("How about some more")
+
+print("Is it grater?",5>2)
+print("Is it grater or equal?",5>=2)
+print("Is it less or equal",5<=2)
