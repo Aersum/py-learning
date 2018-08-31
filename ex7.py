0f8c5477cdfa11d6253a8c615ab68c9c9c56fd05
Anton Ivanov <41456769+Aersum@users.noreply.github.com>
2018-08-31 16:55:34 +0300

    Ex7 complete. Rached page 60

---
+++ ex7.py
@@ -0,0 +1,24 @@
+print("Mary had a little lamb.")
+#printing formatting string
+print("It's fleece was white as {}.".format('snow'))
+print("Any eveywhere that Mary went.")
+#multiplying string ".". Prining 10 dots
+print("."*10)
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#watch that comma at the end.try rempving it to see what happens
+#replaces a new line with a space
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
