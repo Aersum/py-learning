64e240d982d3bdd2c4f703b087c56bb31c137dbb
Antonio <antoniosivanovs@gmail.com>
2018-09-01 20:32:36 +0300

    Ex11 complete. Reached page 70

---
+++ ex11.py
@@ -0,0 +1,11 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weight?", end=' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+hobby = input("What is your hobby: ")
+eat = input("What is your favorite food: ")
+print(f"So, you like {hobby} and eat {eat})))")
