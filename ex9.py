71aaa3bed21109453f6a5a0d6bd79f31341d887c
Antonio <antoniosivanovs@gmail.com>
2018-09-01 19:21:50 +0300

    Ex9 complete. Reached page 63

---
+++ ex9.py
@@ -0,0 +1,16 @@
+
+#creating vars days (one line) and
+#months (many lines because of "\n symbol") 
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\n Feb\nMar\nApr\nMay\nJun\nJul\nAug"
+# printing this vars with some text before
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+#printing some text with saving format of writen
+#because it have written in """ quotes
+print("""
+There's something going on here.
+With the three double qoutes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
