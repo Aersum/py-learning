00adc315952f6af78506ad6fd903357ab635bd5f
Antonio <antoniosivanovs@gmail.com>
2018-09-18 03:33:39 +0300

    Trying #2 to crate a byte file by reading bytes from text fie. It's not work:-(. Move on. Skip the ex23

--- ex23/ex23_rev.py
+++ ex23/ex23_rev.py
@@ -1,24 +1,14 @@
 import sys
-script, input_decoding,  error = sys.argv
+#script, input_decoding,  error = sys.argv
 
-
-def main(language_file, decoding, errors):
-	line = language_file.readline()
-	
+def bfile_create():
+	file_wbytestr = open('lang_bytes.txt')
+	binary_file = open("raw_bytes_lang.txt", "wb")
+	line = file_wbytestr.readline()
 	if line:
-		print_line(line, decoding, errors)
-		return main(language_file, decoding, errors)
-
-
-def print_line(line, decoding, errors):
-	next_lang = line.strip()
-	print(next_lang)
-	jstring = next_lang.decode()
-	cooked_string = jstring.encode(decoding, errors = errors)
-	
-	print(jstring,'<===>',cooked_string)
-	
-	
-languages = open("lang_bytes.txt",encoding="utf-8")
-
-main(languages, input_decoding, error)
+		binary_file.write(line)
+		return bfile_create()
+	bynary_file.close()
+	file_wbytestr.close()
+	print('OK')
+bfile_create()
